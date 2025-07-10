a7eb6cea8defb2c6f8439311bbf42be22d75a1e6
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-10 02:32:09 +0200

    setting files up

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => { 
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/hello', (req, res) => {
+    res.json({ greeting: 'hello API' });
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Your app is listening on port ${listener.address().port}`);
+});
