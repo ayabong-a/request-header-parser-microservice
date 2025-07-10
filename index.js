12234dd5ba01209712de23da95aa5f00a46da514
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-10 03:02:45 +0200

    setup complete

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/hello', (req, res) => {
-    res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const language = req.headers['accept-language'];
+    const software = req.headers['user-agent'];
+    res.json({
+        ipaddress: ip,
+        language: language,
+        software: software
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
